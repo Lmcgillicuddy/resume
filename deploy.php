3aed6b2d6a8cf7c3dcc7501162d45d943916bb91
Lmcgillicuddy <112345338+Lmcgillicuddy@users.noreply.github.com>
2022-08-30 09:59:51 -0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -22,7 +22,7 @@
     foreach($commands AS $command){
         $tmp = shell_exec($command);
         
-        $output .= '<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />';
+        $output .= '\${$command}\n<br />';
         $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
     }
 ?>
